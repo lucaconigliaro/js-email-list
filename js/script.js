28fcd90f45e14068d99133b7cdcfcf57e74f8aa8
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-11-18 17:35:36 +0100

    bonus ended

--- js/script.js
+++ js/script.js
@@ -4,17 +4,18 @@
 // **Bonus**
 // - Abbellire con CSS o Bootstrap
 // - Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+
 const listElem = document.getElementById("mail-list");
-const buttonElem = document.getElementById("refresh")
+const buttonElem = document.getElementById("refresh");
 
 // Ciclo for per generare l'email
 const newEmails = (numEmails) => {
     for (let i = 1; i <= numEmails; i++) {
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-            console.log(resp);
-            listElem.innerHTML += `<li>${resp.data.response}</li>`;
+            // console.log(resp);
+            listElem.innerHTML += `<li class="list-group-item list-group-item-primary">${resp.data.response}</li>`;
         });
-    }
+    };
 };
 
 // Chiamata iniziale per la lista con 10 email
